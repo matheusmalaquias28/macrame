44cf0c67e50efdff40e6c9859c2f3cb9ea172574
matheusmalaquias28 <contato.matheusmalaquias@gmail.com>
2026-07-22 01:04:53 -0300

    ajustes depoimentos

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 /** Links de checkout (Hotmart). */
-export const CHECKOUT_BASIC = "https://pay.hotmart.com/J106763752O?off=3e3jwj1m";
-export const CHECKOUT_COMPLETE = "https://pay.hotmart.com/J106763752O?bid=1784173503754";
+export const CHECKOUT_BASIC = "https://pay.hotmart.com/H106817238V?off=gju0zvti";
+export const CHECKOUT_COMPLETE = "https://pay.hotmart.com/H106817238V?off=b1yc9k18";
