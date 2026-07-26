63472d12dfbb3770fd312168be44c7bf8e07c4c1
matheusmalaquias28 <contato.matheusmalaquias@gmail.com>
2026-07-26 17:16:32 -0300

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 /** Links de checkout (Hotmart). */
-export const CHECKOUT_BASIC = "https://pay.hotmart.com/H106817238V?off=gju0zvti";
-export const CHECKOUT_COMPLETE = "https://pay.hotmart.com/H106817238V?off=b1yc9k18";
+export const CHECKOUT_BASIC = "https://pay.hotmart.com/H106817238V?off=gju0zvti&checkoutMode=10&bid=1785096961456";
+export const CHECKOUT_COMPLETE = "https://pay.hotmart.com/H106817238V?off=b1yc9k18&checkoutMode=10";
